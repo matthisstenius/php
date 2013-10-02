d2312ad9ad5f0e0cf84ab20a1f728beff6a428a7
Matthis Stenius <matthis@me.com>
2013-10-02 21:20:52 +0200

    fixed session hijacking bug

--- labb1/src/view/SessionProtect.php
+++ labb1/src/view/SessionProtect.php
@@ -5,15 +5,13 @@ namespace view;
 class SessionProtect {
 	private static $sessionTest = "view::SessionProtect::sessionTest";
 	private static $userAgent = "view::SessionProtect::userAgent";
-	private static $ip = "view::SessionProtect::ip";
 
 	/**
 	 * @throws Exception if session thef
 	 */
 	public function checkSessionTheft() {
 		if (isset($_SESSION[self::$sessionTest])) {
-			if ($_SESSION[self::$sessionTest][self::$userAgent] != $_SERVER['HTTP_USER_AGENT']
-				|| $_SESSION[self::$sessionTest][self::$ip] != $_SERVER['REMOTE_ADDR']) {
+			if ($_SESSION[self::$sessionTest][self::$userAgent] != $_SERVER['HTTP_USER_AGENT']) {
 				throw new \Exception("Session theft");
 			}	
 		}
@@ -21,7 +19,6 @@ class SessionProtect {
 		else {
 			$_SESSION[self::$sessionTest] = array();
 			$_SESSION[self::$sessionTest][self::$userAgent] = $_SERVER['HTTP_USER_AGENT'];
-			$_SESSION[self::$sessionTest][self::$ip] = $_SERVER['REMOTE_ADDR'];	
 		}
 	}
 }
